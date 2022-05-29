e06ebeb16ce67b4d18e89d2058187358d9f88a4b
kennxdy <kennedy01101@gmail.com>
2022-05-29 01:18:09 -0300

    Refactor code

--- linstairs/linstairs.py
+++ linstairs/linstairs.py
@@ -1,37 +1,34 @@
-from bs4 import BeautifulSoup as Soup
 from urllib.request import urlopen
 
+from bs4 import BeautifulSoup
 
-banner = r'''
-  _____  _     _                   _____             _    
- |  __ \(_)   | |                 |  __ \           | |   
+
+banner = r"""
+  _____  _     _                   _____             _
+ |  __ \(_)   | |                 |  __ \           | |
  | |  | |_ ___| |_ _ __ ___  ___  | |__) |__ _ _ __ | | __
  | |  | | / __| __| '__/ _ \/ __| |  _  // _` | '_ \| |/ /
- | |__| | \__ \ |_| | | (_) \__ \ | | \ \ (_| | | | |   < 
+ | |__| | \__ \ |_| | | (_) \__ \ | | \ \ (_| | | | |   <
  |_____/|_|___/\__|_|  \___/|___/ |_|  \_\__,_|_| |_|_|\_\
-                                                          
 
-'''
+
+"""
+
 
 def search_rank():
     url = 'https://distrowatch.com/'
-
     with urlopen(url) as response:
         html = response.read()
-        page = Soup(html, 'html.parser')
-        distros = page.find_all("td", class_="phr2")
-        pos = 1
-        print('------------------')
-
+        page = BeautifulSoup(html, 'html.parser')
+        distros = page.find_all('td', class_='phr2')
+        position = 1
         for i in range(number):
-            print("|{}| {}".format(pos, distros[i].text))
-            print("------------------")
-            pos += 1
+            print("|{}| {}".format(position, distros[i].text))
+            position += 1
 
-            
+
 if __name__ == '__main__':
     print(banner)
     number = int(input("Enter the number of positions: "))
-    print("Please wait while we fetch the data for you...")
-    print('')
+    print("Please wait while we fetch the data for you...\n")
     search_rank()
